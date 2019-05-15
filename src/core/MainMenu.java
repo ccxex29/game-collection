59980df86a88f5667a7217e84db475bf4c78ed6f
Louis Raymond <blackccxex@gmail.com>
2019-05-15 12:36:47 +0700

    Added RegLogin class and adapted Menu Selection behaviour to RegLogin

--- src/core/MainMenu.java
+++ src/core/MainMenu.java
@@ -6,7 +6,7 @@ public class MainMenu {
 		// TODO Auto-generated constructor stub
 		System.out.println("Game Collection Menu v" + (new GameVersion()).getVersion());
 		
-		System.out.println("Louis Raymond (2201849535) -- Hello World");
+		System.out.println("Louis Raymond (2201849535) -- Hello World (FooBar)");
 		System.out.println("1. Register/Login");
 		System.out.println("2. Play Games");
 		System.out.println("3. Exit");
