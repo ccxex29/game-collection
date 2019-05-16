00f363ef71d166b149b797c09caca00120408589
Louis Raymond <blackccxex@gmail.com>
2019-05-16 12:50:29 +0700

    GameVersion changed for author and author nim

--- src/core/GameVersion.java
+++ src/core/GameVersion.java
@@ -2,12 +2,25 @@ package core;
 
 public class GameVersion{
 	private double version = 0.02D;
-	private String author = "";
+	private String author1 = "Louis Raymond";
+	private String author2 = "Christina Angelia";
+	private String nim1 = "2201849535";
+	private String nim2 = "2201728004";
 	public double getVersion() {
 		return version;
 	}
-	public String getAuthor() {
-		return author;
+	public String getAuthor1() {
+		return author1;
 	}
+	public String getAuthor2() {
+		return author2;
+	}
+	public String getNim1() {
+		return nim1;
+	}
+	public String getNim2() {
+		return nim2;
+	}
+	
 	
 }
--- src/core/MainMenu.java
+++ src/core/MainMenu.java
@@ -4,9 +4,10 @@ public class MainMenu {
 	
 	public MainMenu() {
 		// TODO Auto-generated constructor stub
-		System.out.println("Game Collection Menu v" + (new GameVersion()).getVersion());
+		GameVersion gv = new GameVersion();
+		System.out.println("Game Collection Menu v" + gv.getVersion());
 		
-		System.out.println("Louis Raymond (2201849535) -- Hello World (FooBar)");
+		System.out.println(gv.getAuthor1() + " (" + gv.getNim1() + ") -- " + gv.getAuthor2() + " (" + gv.getNim2() + ")");
 		System.out.println("1. Register/Login");
 		System.out.println("2. Play Games");
 		System.out.println("3. Exit");
